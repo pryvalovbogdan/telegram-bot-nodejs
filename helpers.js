891e68818fc4f7159012be64279279d9b27d8882
bogdan pryvalov <bohdan.privalov@tech-stack.io>
2021-03-09 14:17:07 +0200

    added getAnagram func

--- helpers.js
+++ helpers.js
@@ -1,3 +1,66 @@
+const dictionary = require('websters-english-dictionary');
 const debug = obj => JSON.stringify(obj, null, 4);
 
-module.exports = debug;
+const factorial = num => {
+	if (num === 0) return 1;
+
+	return num * factorial(num - 1);
+};
+
+const getAnagram = word => {
+	console.log('word', word);
+	if (!word || word.includes(' ') || word.length > 8) {
+		return ['no spaces, length of word less 8 symbols and more than 3'];
+	}
+
+	const words = Object.keys(dictionary.dictionary());
+
+	const wordsSameLength = words.filter(item =>
+		item.length <= word.length && item.length >= 3 && !item.includes('-'));
+
+	const anagramCases = factorial(word.length);
+	const res = [];
+
+	let tempWord = word.split('');
+
+	for (let i = 0; i < anagramCases; i++ ) {
+		for (let j = 0; j < word.length; j++) {
+			for (let l = 1; l < word.length + 1; l++) {
+				const temp = tempWord[j];
+				tempWord[j] = tempWord[l];
+				tempWord[l] = temp;
+
+				const someWord = tempWord.filter(el => el != null).join('');
+
+				if (!res.includes(someWord)) {
+					res.push(someWord)
+				}
+			}
+		}
+	}
+
+	const count = word.length - 3;
+	const temp2 = [...res];
+
+	for (let e = 0; e <= count; e++) {
+		for (let k = 0; k < res.length; k++) {
+			if (temp2[k] && !res.includes(temp2[k].slice(0, temp2[k].length - e))) {
+				res.push(temp2[k].slice(0, temp2[k].length - e))
+			}
+		}
+	}
+
+	const finalRes = [];
+	wordsSameLength.forEach(item => res.forEach(some => {
+		if (item === some && item !== word) {
+			finalRes.push(item)
+		}
+	}));
+
+	return finalRes;
+};
+
+module.exports = {
+	debug,
+	getAnagram,
+};
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
 console.log('Bot have been started');
-const token = '1662367182:AAHUwIfdJTSqChGj4ijupXSL9XrEbOkfNms';
-const debug = require('./helpers');
+const token = '1560965662:AAExJkGoMuUMnzBgGuVgsRkEdqDWB-fb0q0';
+const { debug, getAnagram } = require('./helpers');
+const fs = require('fs');
+
 const bot = new TelegramBot(token, {
   polling: {
     interval: 300,
@@ -12,18 +14,29 @@ const bot = new TelegramBot(token, {
   },
 });
 
-bot.onText(/\/start/, msg => {
-  const { id } = msg.chat;
-
-  bot.sendMessage(id, `Zdravstvyi pizduk ${msg.from.first_name} https://i.pinimg.com/originals/e1/10/fd/e110fde3397a6562f298bbb523720145.gif`);
-});
 const endpoints = [ { name: '/start', description: 'start to work with bot'},
+  { name: '/anagram word', description: 'put any word to get anagram of it'},
   { name: '/helppizdyk', description: 'to see all commands'},
   { name: '/porabodyashitb', description: 'work on debug'},
   { name: '/endpizdesh', description: 'stop working'},
-  { name: '/checkpizduk', description: 'are you pizduk'},
   { name: '/inlinekeyboard', description: 'check inline'},
 ];
+
+bot.onText(/\/start/, msg => {
+  const { id } = msg.chat;
+  const html = `
+    <strong>Zdravstvyi pizduk ${msg.from.first_name}</strong>
+    <strong>Commands: </strong>
+    ${endpoints.map(item => `
+      <b>${item.name}</b><b> - ${item.description}</b>`)}
+    `;
+
+  bot.sendAnimation(id, 'https://i.pinimg.com/originals/e1/10/fd/e110fde3397a6562f298bbb523720145.gif', {
+    caption: html,
+    duration: 0,
+    parse_mode: 'HTML',
+  });
+});
 
 bot.onText(/\/helppizdyk/, msg => {
   const { id } = msg.chat;
@@ -41,14 +54,17 @@ bot.onText(/\/helppizdyk/, msg => {
 bot.onText(/\/endpizdesh/, msg => {
   const { id } = msg.chat;
 
-  bot.sendMessage(id, `Poka rodnoy ${msg.from.username} https://media0.giphy.com/media/UU2VKeTNOgFFoYiAIp/giphy.gif`);
+  bot.sendPhoto(id, fs.readFileSync(__dirname + '/assets/robot.jpg'), {
+    caption: `Poka rodnoy ${msg.from.username}`
+  });
 });
 
 bot.onText(/\/porabodyashitb/, msg => {
   const { id } = msg.chat;
 
-  bot.sendMessage(id, 'Zavarivay 4ayek mi na4inaem, https://media1.giphy.com/media/xTiTnpZBALuuuwyzw4/giphy.gif', {
-    disable_notification: true,
+  bot.sendAnimation(id, 'https://media1.tenor.com/images/a37427a9b3ef5d3d121c9092be96543a/tenor.gif?itemid=5606830', {
+    caption: `Zavarivay 4ayek mi na4inaem /helppizdyk`,
+    duration: 0,
   });
 });
 
@@ -99,7 +115,7 @@ bot.onText(/\/inlinekeyboard/, msg => {
 });
 
 bot.on('callback_query', query => {
-  // bot.sendMessage(query.message.chat.id, debug(query));
+  bot.sendMessage(query.message.chat.id, debug(query));
   const  { chat, message_id, text } = query.message;
 
   switch(query.data) {
@@ -111,6 +127,12 @@ bot.on('callback_query', query => {
         reply_to_message_id: message_id
       });
       break;
+    case 'Edit' :
+      bot.editMessageText(`${text} edited`, {
+        chat_id: chat.id,
+        message_id,
+      });
+      break;
     default:
       break;
   }
@@ -120,7 +142,7 @@ bot.on('callback_query', query => {
 
 bot.on('message', (msg) => {
   const { id } = msg.chat;
-  // console.log('msg', msg);
+
   switch (msg.text) {
     case 'no':
       bot.sendMessage(id, 'close keyboard', {
@@ -139,4 +161,24 @@ bot.on('message', (msg) => {
     default:
       break;
   }
+});
+
+bot.onText(/\/anagram (.+)/, (msg, data) => {
+  const { id } = msg.chat;
+  const anagrams = getAnagram(data[1]);
+  const html = `
+    <strong>Anagrams from webster dictionary: </strong>
+    ${anagrams.length ? anagrams.map(item => `<b>${item}</b> `) : 'There is no matching'}
+  <strong>/helppizdyk</strong>
+    `;
+
+  bot.sendAnimation(id, 'https://media1.giphy.com/media/xTiTnpZBALuuuwyzw4/giphy.gif', {
+    caption: html,
+    duration: 0,
+    parse_mode: 'HTML',
+  });
+});
+
+bot.on('polling_error', (error) => {
+  console.log(error.code);  // => 'EFATAL'
 });
